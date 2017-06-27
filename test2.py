f71f740a52a80dcb9562cd770235d3d2c20dfb61
wuhongle <wuhongle@hotmail.com>
2017-06-27 17:17:12 +0900

    create 1.py

---
+++ test2.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+
+matrix1 = tf.constant([[3,3]])
+matrix2 = tf.constant([[2],
+                       [2]])
+product = tf.matmul(matrix1,matrix2)
+
+sess = tf.Session()
+result = sess.run(product)
+print(result)
+sess.close()
+# [[12]]
+
+# method 2
+with tf.Session() as sess:
+    result2 = sess.run(product)
+    print(result2)
+# [[12]]
